fffdade6ffc2b535d9a3944d4956f4559b4dac13
Mayuri Gaikwad <mayurig@example.com>
2026-06-01 21:46:23 +0530

    Basic_to_advance_python

---
+++ Python_basic_to_advance/modules/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,3 @@
+from package.maths import*
+print(addition(2,3))
+print(substraction(4,3))
---
+++ Python_basic_to_advance/modules/test.py
@@ -0,0 +1,5 @@
+from package.maths import*
+from package.subpackage.mult import multiply
+print(addition(2,3))
+print(substraction(4,3))
+print(multiply(4,5))
